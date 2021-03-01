c20bf9e848729ee94327f718775429f9bff52553
Tuna <df95111111>
2021-03-01 17:47:50 +0800

    更新admin畫面

--- database/migrations/2021_03_01_072845_create_flights_table.php
+++ database/migrations/2021_03_01_072845_create_flights_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateFlightsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('flights', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('deposit')->comment('剩餘金額');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('flights');
     }
 }
