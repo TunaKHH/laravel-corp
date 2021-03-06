e58cd355950a50b71bb8d6d179c96a7c73fd32b9
Tuna <s15113114@stu.edu.tw>
2021-03-06 22:13:21 +0800

    加入datatable,livewire

---
+++ app/Http/Livewire/Lunch.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Lunch extends Component
+{
+    public function render()
+    {
+        return view('livewire.lunch');
+    }
+}
