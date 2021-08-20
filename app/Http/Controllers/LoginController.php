2b10f0131bb365a758fee38c40384e8d616e66eb
Tuna <df95111111@gmail.com>
2021-08-20 11:31:58 +0800

    利用email查詢帳號

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Services\LineService;
 use Illuminate\Support\Facades\Auth;
+use function Deployer\get;
 
 
 class LoginController extends Controller
@@ -37,9 +39,30 @@ class LoginController extends Controller
 
     public function authenticate(Request $request)
     {
-        $credentials = $request->only('account', 'password');
 
-        if (!Auth::attempt($credentials,$request->remember)) {
+//        $credentials = $request->only('account', 'password');# 帳號登入
+        $account = $request->get('account');
+        $password = $request->get('password');
+
+        $user = User::where('account',$account)->get()->first(); // 確認有沒有這個帳號
+
+        if(!$user){// 沒這個帳號 用email查account
+            $user = User::where('email',$account)->get()->first(); // 確認有沒有這個帳號
+            if($user){
+                $account = $user->account;
+            }
+        }
+
+
+        $credentials = ['account'=>$account, 'password'=>$password];
+
+        # 使用email找帳號
+
+        $is_login = false;
+
+        # 兩種登入方式
+        if (!$is_login && !Auth::attempt($credentials,$request->remember)) {
+
             return back()->withErrors([
                 'account' => '帳號或密碼錯誤',
             ]);
