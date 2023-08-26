c4fd653311c2d9de4c907db8d75e80c6d4486dd9
STUTuna <df95111111@gmail.com>
2023-08-26 14:53:58 +0800

    refactor: 分離全行文字function到helper, 重構LineService內部函數

--- app/Services/LineService.php
+++ app/Services/LineService.php
@@ -34,7 +34,7 @@ class LineService
         $is_order = $this->isOrderInfo($command);
         // 判斷文字指令是否為刪除
         $is_del = $this->isDelInfoBy($command);
-        $command = $this->nf_to_wf($command, 'wf_to_nf');
+        $command = nf_to_wf($command, 'wf_to_nf');
         if (in_array($command, self::CONFIRM_COMMANDS)) {
             return $this->confirmOrder($last_task);
         } elseif (in_array($command, self::CHECK_BALANCE_COMMANDS)) {
@@ -60,48 +60,75 @@ class LineService
      */
     private function confirmOrder(Task $last_task)
     {
-        $res = '';
-        $order_processed = '';
-        $info_list = '';
-        $money = 0;
-        $groupby_foodname = array();
         $order_list = $last_task->taskOrder()->get();
-
-        // 判斷$order_list 是否為空
         if ($order_list->isEmpty()) {
-            $info_list = '尚無點餐';
-            return $info_list;
+            return '尚無點餐';
         }
+
+        $info_list = $this->formatOrderList($order_list);
+        $money = $this->calculateTotalMoney($order_list);
+        $order_processed = $this->processOrderGroupByFood($order_list);
+
+        $res = '訂單建立時間:' . self::WRAP_STR . $last_task->create_time . self::WRAP_STR;
+        $res .= $info_list . self::WRAP_STR . '----訂單整理----' . self::WRAP_STR;
+        $res .= $order_processed . self::WRAP_STR . '總金額：' . $money;
+
+        return $res;
+    }
+
+    private function formatOrderList($order_list)
+    {
+        $info_list = '';
         foreach ($order_list as $info) {
-            // 點餐列表
-            $oneLine = [];
-            array_push($oneLine, $info->id);
-            array_push($oneLine, $info->user->nickname ?? $info->user->name);
-            array_push($oneLine, $info->meal_name);
-            array_push($oneLine, $info->meal_price);
-            array_push($oneLine, '數量:' . $info->qty);
-            array_push($oneLine, $info->remark);
+            $oneLine = [
+                $info->id,
+                $info->user->nickname ?? $info->user->name,
+                $info->meal_name,
+                $info->meal_price,
+                '數量:' . $info->qty,
+                $info->remark,
+            ];
             $info_list .= implode(' ', $oneLine) . self::WRAP_STR;
+        }
+        return $info_list;
+    }
 
-            // 計算金額
+    /**
+     * 計算總金額
+     *
+     * @param [type] $order_list
+     * @return int|float
+     */
+    private function calculateTotalMoney($order_list)
+    {
+        $money = 0;
+        foreach ($order_list as $info) {
             $info_money = $info['meal_price'] * $info['qty'];
-            $money = $info_money + $money;
-            // 點餐統整
+            $money += $info_money;
+        }
+        return $money;
+    }
+
+    /**
+     * 處理訂單
+     *
+     * @param [type] $order_list
+     * @return string
+     */
+    private function processOrderGroupByFood($order_list)
+    {
+        $groupby_foodname = [];
+        foreach ($order_list as $info) {
             $meal_name = $info['meal_name'];
-
             $groupby_foodname[$meal_name] = ($groupby_foodname[$meal_name] ?? 0) + $info->qty;
-
         }
         ksort($groupby_foodname);
 
+        $order_processed = '';
         foreach ($groupby_foodname as $name => $count) {
             $order_processed .= $name . ':' . $count . self::WRAP_STR;
         }
-        $res = '訂單建立時間:' . self::WRAP_STR . $last_task->create_time . self::WRAP_STR . $info_list . self::WRAP_STR;
-        $res .= '----訂單整理----' . self::WRAP_STR;
-        $res .= $order_processed . self::WRAP_STR;
-        $res .= '總金額：' . $money;
-        return $res;
+        return $order_processed;
     }
 
     /* 開啟點餐
@@ -119,11 +146,7 @@ class LineService
         // 開啟訂單團
         $task = new Task;
         $task->line_group_id = $groupId;
-        $save_res = $task->save();
-        if ($save_res) {
-            return '好,開啟了,請直接點餐(EX:鮪 炒飯 $100 不要蔥)';
-        }
-        return '開啟訂單失敗';
+        return $task->save() ? '好,開啟了,請直接點餐(EX:鮪 炒飯 $100 不要蔥)' : '開啟訂單失敗';
     }
 
     private function checkBalance()
@@ -147,29 +170,11 @@ class LineService
     {
         if (isset($last_task) && $last_task->is_open !== 1) { // 確認最新訂單是否已關閉(上次的)
             return '已經結束過';
-        } else {
-            // 關閉訂單
-            $last_task->is_open = 2;
-            $last_task->save();
-            return '已關閉';
         }
-    }
-
-    /**
-     * 刪除餐點
-     *
-     * @param string $command
-     * @return string
-     */
-    private function delOrder($command)
-    {
-        $info_id = explode(" ", $command)[1];
-        $taskOrder = TaskOrder::find($info_id);
-        // 要刪除的餐點名稱
-        $meal_name = $taskOrder->meal_name;
-        $taskOrder->delete();
-        $res = '已刪除' . self::WRAP_STR . $meal_name;
-        return $res;
+        // 關閉訂單
+        $last_task->is_open = 2;
+        $last_task->save();
+        return '已關閉';
     }
 
     // 取得我的lineId
@@ -181,27 +186,55 @@ class LineService
     // 訂餐
     public function orderMeal($command, Task $last_task, $groupId)
     {
-        // 根據lineId取得user
         $user = User::getUserByLineId($this->userLineId);
-        // 判斷user是否存在
         if (!$user) {
             return '請先在訂餐網頁綁定Line帳號';
         }
 
-        // 新增點餐
-        $OrderTask = new TaskOrder;
-        $OrderTask->user_id = $user->id; // 設定user_id
-        $OrderTask->task_id = $last_task->id; // 設定task_id
-        $OrderTask->meal_name = explode(" ", $command)[1]; // 設定餐點名稱
-        $OrderTask->meal_price = explode("$", explode(" ", $command)[2])[1]; // 設定餐點價格
-        $OrderTask->qty = 1; // 設定餐點數量
-        $OrderTask->remark = explode(" ", $command)[3] ?? ''; // 設定餐點備註
-        if ($OrderTask->save()) {
-            return '收';
-        }
-        return '點餐失敗';
+        $OrderTask = $this->createTaskOrder($command, $last_task, $user);
+        return $OrderTask->save() ? '收' : '點餐失敗';
     }
 
+    private function createTaskOrder($command, Task $last_task, $user)
+    {
+        $OrderTask = new TaskOrder;
+        $OrderTask->user_id = $user->id;
+        $OrderTask->task_id = $last_task->id;
+        list($meal_name, $meal_price, $remark) = $this->parseOrderCommand($command);
+        $OrderTask->meal_name = $meal_name;
+        $OrderTask->meal_price = $meal_price;
+        $OrderTask->qty = 1;
+        $OrderTask->remark = $remark;
+        return $OrderTask;
+    }
+
+    private function parseOrderCommand($command)
+    {
+        $parts = explode(" ", $command);
+        $meal_name = $parts[1];
+        $meal_price = explode("$", $parts[2])[1];
+        $remark = $parts[3] ?? '';
+        return [$meal_name, $meal_price, $remark];
+    }
+
+    /**
+     * 刪除餐點
+     *
+     * @param string $command
+     * @return string
+     */
+    public function delOrder($command)
+    {
+        $info_id = explode(" ", $command)[1];
+        $taskOrder = TaskOrder::find($info_id);
+        if (!$taskOrder) {
+            return '找不到要刪除的餐點';
+        }
+
+        $meal_name = $taskOrder->meal_name;
+        $taskOrder->delete();
+        return '已刪除' . self::WRAP_STR . $meal_name;
+    }
     // 取得我的lineId
     public function getMyLineId()
     {
@@ -263,51 +296,6 @@ class LineService
             }
         }
         return $res;
-    }
-
-    /* 全形半形轉換
-     * @param string $strs
-     * @param string $types
-     * @return string
-     */
-    public function nf_to_wf($strs, $types = null)
-    {
-        $nft = array(
-            "(", ")", "[", "]", "{", "}", ".", ",", ";", ":",
-            "-", "?", "!", "@", "#", "$", "%", "&", "|", "\\",
-            "/", "+", "=", "*", "~", "`", "'", "\"", "<", ">",
-            "^", "_", "[", "]",
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-            "u", "v", "w", "x", "y", "z",
-            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-            "U", "V", "W", "X", "Y", "Z",
-            " ",
-        );
-        $wft = array(
-            "（", "）", "〔", "〕", "｛", "｝", "﹒", "，", "；", "：",
-            "－", "？", "！", "＠", "＃", "＄", "％", "＆", "｜", "＼",
-            "／", "＋", "＝", "＊", "～", "、", "、", "＂", "＜", "＞",
-            "︿", "＿", "【", "】",
-            "０", "１", "２", "３", "４", "５", "６", "７", "８", "９",
-            "ａ", "ｂ", "ｃ", "ｄ", "ｅ", "ｆ", "ｇ", "ｈ", "ｉ", "ｊ",
-            "ｋ", "ｌ", "ｍ", "ｎ", "ｏ", "ｐ", "ｑ", "ｒ", "ｓ", "ｔ",
-            "ｕ", "ｖ", "ｗ", "ｘ", "ｙ", "ｚ",
-            "Ａ", "Ｂ", "Ｃ", "Ｄ", "Ｅ", "Ｆ", "Ｇ", "Ｈ", "Ｉ", "Ｊ",
-            "Ｋ", "Ｌ", "Ｍ", "Ｎ", "Ｏ", "Ｐ", "Ｑ", "Ｒ", "Ｓ", "Ｔ",
-            "Ｕ", "Ｖ", "Ｗ", "Ｘ", "Ｙ", "Ｚ",
-            "　",
-        );
-
-        if ($types == 'nf_to_wf') { // 轉全形
-            return str_replace($nft, $wft, $strs);
-        } else if ($types == 'wf_to_nf') { // 轉半形
-            return str_replace($wft, $nft, $strs);
-        } else {
-            return $strs;
-        }
     }
 
     /**
