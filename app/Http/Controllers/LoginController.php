f82d782658d7abce5afa928cfc312da1f54aef13
STUTuna <df95111111@gmail.com>
2023-09-13 19:55:31 +0800

    refactor(LoginController): remove unused code

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Services\LineService;
-use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
@@ -82,21 +81,4 @@ class LoginController extends Controller
         return view('line')->with('url', $url);
     }
 
-    public function lineLoginCallBack(Request $request)
-    {
-        try {
-            $error = $request->input('error', false);
-            if ($error) {
-                throw new Exception($request->all());
-            }
-            $code = $request->input('code', '');
-            $response = $this->lineService->getLineToken($code);
-            $user_profile = $this->lineService->getUserProfile($response['access_token']);
-            echo "<pre>";
-            print_r($user_profile);
-            echo "</pre>";
-        } catch (Exception $ex) {
-            Log::error($ex);
-        }
-    }
 }
